be8f47df0213f7fbe4f94982c48ce315c525bbd2
anicolellis <alexnicolellis1@gmail.com>
2026-02-03 19:49:54 -0600

    book list creation with simple format

---
+++ Frontend/EventLog-Website/src/pages/BookListPage.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+
+export function BookListPage() {
+    const [books, setBooks] = useState([]);
+    // Get book list when loading the page
+    useEffect(() => {
+        axios.get()
+            .then((response) => {
+                setBooks(response.data);
+            });
+    }, []);
+
+    return (
+        <>
+            <title>Alex's Book Log</title>
+            <div>
+                {books.map((book) => {
+                    <div key={book.id}>
+                        <div>
+                            {`${book.title} by ${book.author}`}
+                        </div>
+                        <div>
+                            {`Date finished: ${book.completion}`}
+                        </div>
+                    </div>
+                })}
+            </div>
+        </>
+    );
+}
